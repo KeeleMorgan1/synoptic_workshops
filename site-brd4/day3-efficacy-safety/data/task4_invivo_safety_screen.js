fdfd66e66ca4955296252f5cc11d984ad880fdce
KeeleMorgan1 <dgamorgan@gmail.com>
2026-09-16 18:20:16 +0100

    Added BRD4 files

---
+++ site-brd4/day3-efficacy-safety/data/task4_invivo_safety_screen.js
@@ -0,0 +1,152 @@
+window.TASK4_DATA = [
+  {
+    "ID": "C19",
+    "Series": "C",
+    "Rat NOAEL (mg/kg/d)": 15,
+    "Safety Margin (NOAEL/ED)": 0.5,
+    "BM WBC Δ% @30mg/kg": -54,
+    "Body Wt Δ% @30mg/kg": -12,
+    "hERG IC50 (uM)": 61,
+    "hERG Safety Margin": 28005
+  },
+  {
+    "ID": "C20",
+    "Series": "C",
+    "Rat NOAEL (mg/kg/d)": 10,
+    "Safety Margin (NOAEL/ED)": 0.3,
+    "BM WBC Δ% @30mg/kg": -46,
+    "Body Wt Δ% @30mg/kg": -6,
+    "hERG IC50 (uM)": 52,
+    "hERG Safety Margin": 31759
+  },
+  {
+    "ID": "C22",
+    "Series": "C",
+    "Rat NOAEL (mg/kg/d)": 26,
+    "Safety Margin (NOAEL/ED)": 0.9,
+    "BM WBC Δ% @30mg/kg": -38,
+    "Body Wt Δ% @30mg/kg": -16,
+    "hERG IC50 (uM)": 39,
+    "hERG Safety Margin": 24947
+  },
+  {
+    "ID": "C23",
+    "Series": "C",
+    "Rat NOAEL (mg/kg/d)": 14,
+    "Safety Margin (NOAEL/ED)": 0.5,
+    "BM WBC Δ% @30mg/kg": -49,
+    "Body Wt Δ% @30mg/kg": -10,
+    "hERG IC50 (uM)": 32,
+    "hERG Safety Margin": 21235
+  },
+  {
+    "ID": "C25",
+    "Series": "C",
+    "Rat NOAEL (mg/kg/d)": 77,
+    "Safety Margin (NOAEL/ED)": 2.6,
+    "BM WBC Δ% @30mg/kg": -24,
+    "Body Wt Δ% @30mg/kg": -13,
+    "hERG IC50 (uM)": 63,
+    "hERG Safety Margin": 26012
+  },
+  {
+    "ID": "D09",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 24,
+    "Safety Margin (NOAEL/ED)": 0.8,
+    "BM WBC Δ% @30mg/kg": -21,
+    "Body Wt Δ% @30mg/kg": -13,
+    "hERG IC50 (uM)": 41,
+    "hERG Safety Margin": 31025
+  },
+  {
+    "ID": "D10",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 33,
+    "Safety Margin (NOAEL/ED)": 1.1,
+    "BM WBC Δ% @30mg/kg": -47,
+    "Body Wt Δ% @30mg/kg": -7,
+    "hERG IC50 (uM)": 24,
+    "hERG Safety Margin": 9217
+  },
+  {
+    "ID": "D11",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 25,
+    "Safety Margin (NOAEL/ED)": 0.8,
+    "BM WBC Δ% @30mg/kg": -57,
+    "Body Wt Δ% @30mg/kg": -9,
+    "hERG IC50 (uM)": 55,
+    "hERG Safety Margin": 22686
+  },
+  {
+    "ID": "D15",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 11,
+    "Safety Margin (NOAEL/ED)": 0.4,
+    "BM WBC Δ% @30mg/kg": -13,
+    "Body Wt Δ% @30mg/kg": -4,
+    "hERG IC50 (uM)": 39,
+    "hERG Safety Margin": 26443
+  },
+  {
+    "ID": "D16",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 70,
+    "Safety Margin (NOAEL/ED)": 2.3,
+    "BM WBC Δ% @30mg/kg": -9,
+    "Body Wt Δ% @30mg/kg": -3,
+    "hERG IC50 (uM)": 26,
+    "hERG Safety Margin": 12418
+  },
+  {
+    "ID": "D18",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 42,
+    "Safety Margin (NOAEL/ED)": 1.4,
+    "BM WBC Δ% @30mg/kg": -27,
+    "Body Wt Δ% @30mg/kg": -7,
+    "hERG IC50 (uM)": 60,
+    "hERG Safety Margin": 53156
+  },
+  {
+    "ID": "D19",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 100,
+    "Safety Margin (NOAEL/ED)": 3.3,
+    "BM WBC Δ% @30mg/kg": -21,
+    "Body Wt Δ% @30mg/kg": -4,
+    "hERG IC50 (uM)": 66,
+    "hERG Safety Margin": 18395
+  },
+  {
+    "ID": "D20",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 100,
+    "Safety Margin (NOAEL/ED)": 3.3,
+    "BM WBC Δ% @30mg/kg": -15,
+    "Body Wt Δ% @30mg/kg": 2,
+    "hERG IC50 (uM)": 75,
+    "hERG Safety Margin": 18111
+  },
+  {
+    "ID": "D21",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 100,
+    "Safety Margin (NOAEL/ED)": 3.3,
+    "BM WBC Δ% @30mg/kg": -23,
+    "Body Wt Δ% @30mg/kg": 0,
+    "hERG IC50 (uM)": 73,
+    "hERG Safety Margin": 16963
+  },
+  {
+    "ID": "D22",
+    "Series": "D",
+    "Rat NOAEL (mg/kg/d)": 100,
+    "Safety Margin (NOAEL/ED)": 3.3,
+    "BM WBC Δ% @30mg/kg": -18,
+    "Body Wt Δ% @30mg/kg": -5,
+    "hERG IC50 (uM)": 51,
+    "hERG Safety Margin": 25547
+  }
+];
